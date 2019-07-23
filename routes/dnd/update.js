b977a19be34299630fc2d31b04501804fa58ffeb
John Meguerian <johnmeguerian@gmail.com>
2019-07-23 08:55:34 -0400

    Update `/update` route to be universal for all tables, not just stats table.

--- routes/dnd/update.js
+++ routes/dnd/update.js
@@ -7,22 +7,25 @@ Airtable.configure({
 
 const base = Airtable.base('appolcd4hC9nq6bRN')
 
-const updateValue = async (id, value) => {
-  const record = await base('stats').update(id, { value: parseInt(value) })
+// table:String, id:String, payload:Obj
+// pass the name of the table, the record id, and an object containing the
+// fields and values that will be merged into the airtable record.
+const updateValue = async (table, id, payload) => {
+  const record = await base(table).update(id, payload)
   return record
 }
 
 module.exports = async (req, res) => {
-  let { id, value } = req.query
-  console.log(id, value)
+  let { table, id, payload } = req.body
+  console.log(table, id, payload)
 
-  if (!id || !value) {
+  if (!table || !id || !payload) {
     return res.status(400).send('missing param')
   }
 
   try {
-    const record = await updateValue(id, value)
-    return res.status(200).send(`${record.get('name')} updated`)
+    await updateValue(table, id, payload)
+    return res.status(200).send(`${id} updated`)
   } catch (error) {
     console.error(error)
     return res.status(500).send('server error')
