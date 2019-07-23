d3a8a3d230070e3f15a8c1977578efd8154a371a
John Meguerian <johnmeguerian@gmail.com>
2019-07-23 13:30:49 -0400

    Revert to f7b2089

--- routes/dnd/update.js
+++ routes/dnd/update.js
@@ -7,25 +7,22 @@ Airtable.configure({
 
 const base = Airtable.base('appolcd4hC9nq6bRN')
 
-// table:String, id:String, payload:Obj
-// pass the name of the table, the record id, and an object containing the
-// fields and values that will be merged into the airtable record.
-const updateValue = async (table, id, payload) => {
-  const record = await base(table).update(id, payload)
+const updateValue = async (id, value) => {
+  const record = await base('stats').update(id, { value: parseInt(value) })
   return record
 }
 
 module.exports = async (req, res) => {
-  let { table, id, payload } = req.body
-  console.log(table, id, payload)
+  let { id, value } = req.query
+  console.log(id, value)
 
-  if (!table || !id || !payload) {
+  if (!id || !value) {
     return res.status(400).send('missing param')
   }
 
   try {
-    await updateValue(table, id, payload)
-    return res.status(200).send(`${id} updated`)
+    const record = await updateValue(id, value)
+    return res.status(200).send(`${record.get('name')} updated`)
   } catch (error) {
     console.error(error)
     return res.status(500).send('server error')
